98001dfb0455b3f162cba2705c104d85d463ca33
lessia2020 <63756124+lessia2020@users.noreply.github.com>
2023-01-25 10:21:54 +0100

    Add files via upload

---
+++ Hello/Hello/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Hello
+//
+//  Created by Olesia Nurislamova on 25.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let myBgColor = Color(red: 255/255, green: 251/255, blue: 245/255)
+    let lightPurple = Color(red: 195/255, green: 172/255, blue: 208/255)
+    let darkPurple = Color(red: 48/255, green: 30/255 , blue: 103/255)
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            ZStack {
+                Image(systemName: "heart")
+                    .imageScale(.large)
+                    .foregroundColor(darkPurple)
+                    .foregroundColor(.accentColor)
+                Text("Hello, Asado!")
+            }
+            .foregroundColor(Color.white)
+            .font(.system(size: 110))
+            .background(lightPurple)
+            .padding()
+            Spacer()
+            Image("dog")
+                .resizable()
+                .frame(width: 350, height: 300)
+        }
+        myBgColor.ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
